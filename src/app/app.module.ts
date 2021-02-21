c47d3a8bd43215cbcd4e8ce83cdeb1c9245fecca
Elena Diaz Garrido <elena.diazgarrido@gmail.com>
2021-02-21 13:58:10 +0100

    proyecto creado. componentes creados con routing configurado. bootstrap instalado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
